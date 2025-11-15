497dfa446b064a84eed6c32d07330c778d6fd812
LilianaLopezclases <lilianaclases@hotmail.com>
2025-11-15 14:54:34 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,8 @@
+//  Python code
+// 
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    pins.digitalWritePin(DigitalPin.P0, 1)
+})
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    pins.digitalWritePin(DigitalPin.P0, 0)
+})
